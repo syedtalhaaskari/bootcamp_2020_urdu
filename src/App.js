ec7c189d4690440acfb1181c9373e59a28515e90
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-05-01 20:45:17 +0500

    12 27

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
-import RouteConfig from "./RouteConfig";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  useRouteMatch,
+} from "react-router-dom";
+
+const Home = () => (<h1>Hi I'm in home</h1>)
+const Blog = () => (<h1>Hi I'm in blog</h1>)
 
 const App = () => {
   return (
-    <div className="App">
-      <RouteConfig />
-    </div>
+    <Router>
+      <div>
+        <ul className="navbar">
+          <li><NavLink className={useRouteMatch('/').pathname === location.pathname ? 'active' : ''} to="/" >Home</NavLink></li>
+          <li><NavLink className={useRouteMatch('/blog').pathname === location.pathname ? 'active' : ''} to="/blog">Blog</NavLink></li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
