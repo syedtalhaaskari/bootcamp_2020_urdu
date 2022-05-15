9381afb61b752aaad17e89206198daafdc2c6ba6
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-05-15 15:37:04 +0500

    complete tutorial

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
 import "./App.css";
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   NavLink,
-  useRouteMatch,
+  Link,
+  Outlet,
+  useParams,
 } from "react-router-dom";
 
 const Home = () => (<h1>Hi I'm in home</h1>)
-const Blog = () => (<h1>Hi I'm in blog</h1>)
+const BlogStage = () => {
+  return (
+    <div>
+      <h1>Hi I'm in blog</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+const BlogIndex = () => {
+  return (
+    <ul>
+      <li><Link to="authors">Authors</Link></li>
+      <li><Link to="1">Post 1</Link></li>
+      <li><Link to="2">Post 2</Link></li>
+    </ul>
+  )
+}
+
+const BlogAuthors = () => (<h2>Snir David</h2>)
+const BlogPost = () => {
+  const { postId } = useParams()
+  
+  return (
+    <h2>Blog Post {postId}</h2>
+  )
+}
+const Blog2 = () => (<h2>Blog 2</h2>)
 
 const App = () => {
+  const navArr = [
+    {
+      navLink: "/",
+      title: "Home",
+    },
+    {
+      navLink: "/blog",
+      title: "Blog",
+    },
+  ]
+
   return (
-    <Router>
-      <div>
-        <ul className="navbar">
-          <li><NavLink className={useRouteMatch('/').pathname === location.pathname ? 'active' : ''} to="/" >Home</NavLink></li>
-          <li><NavLink className={useRouteMatch('/blog').pathname === location.pathname ? 'active' : ''} to="/blog">Blog</NavLink></li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <ul className="navbar">
+        {
+          navArr.map((item, ind) => (
+            <li key={ind}><NavLink to={item.navLink} end>{item.title}</NavLink></li>
+          ))
+        }
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="blog" element={<BlogStage />} >
+          <Route path="" element={<BlogIndex />} />
+          <Route path="authors" element={<BlogAuthors />} />
+          <Route path=":postId" element={<BlogPost />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
