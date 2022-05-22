395d275bb6be14f9dbf3f023ccf322c79e73b183
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-05-22 10:58:06 +0500

    Complete Tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,112 @@
-const App = () => {
+import {
+  BrowserRouter as Router,
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Session 3</h1>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="launch">Launch</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="launch" element={<Launch />} >
+          <Route path="" element={<LaunchIndex />} />
+          <Route path=":slug" element={<LaunchShoe />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Not found!</h1>
+      <p>Sorry your page was not found!</p>
     </div>
   )
 }
 
-export default App;
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome Home!</h1>
+    </div>
+  )
+}
+
+const Launch = () => {
+  return (
+    <div>
+      <h1>Launch</h1>
+
+      <Outlet />
+    </div>
+  )
+}
+
+const LaunchIndex = () => {
+  return (
+    <ul>
+      {Object.entries(shoes).map(([slug, { name, img }]) => (
+        <li key={slug}>
+          <Link to={`/launch/${slug}`}>
+            <h2>{name}</h2>
+            <img src={img} alt={name} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const LaunchShoe = () => {
+  const { slug } = useParams();
+  const shoe = shoes[slug];
+
+  if (!shoe) {
+    return <h2>Not Found!</h2>
+  }
+
+  const { name, img } = shoe;
+  return <div>
+    <h2>{name}</h2>
+    <img src={img} alt={name} />
+  </div>
+}
+
+const shoes = {
+  "air-jordan-3-valor-blue": {
+    "name": "VALOUR BLUE",
+    "img": "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "jordan-mars-270-london": {
+    "name": "JORDAN MARS 270 LONDON",
+    "img": "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "air-jordan-1-zoom-racer-blue": {
+    "name": "RACER BLUE",
+    "img": "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "air-jordan-3-valor-blue1": {
+    "name": "VALOUR BLUE",
+    "img": "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "jordan-mars-270-london2": {
+    "name": "JORDAN MARS 270 LONDON",
+    "img": "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  },
+  "air-jordan-1-zoom-racer-blue3": {
+    "name": "RACER BLUE",
+    "img": "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
+  }
+}
